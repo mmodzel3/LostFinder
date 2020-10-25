ce4d3a699a30b1118b244e863fc7df8bbd115311
mmodzel3 <45737456+mmodzel3@users.noreply.github.com>
2020-10-25 16:50:06 +0100

    Added OpenStreetMap.

--- app/src/main/java/com/github/mmodzel3/lostfinder/MainActivity.kt
+++ app/src/main/java/com/github/mmodzel3/lostfinder/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.github.mmodzel3.lostfinder
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.preference.PreferenceManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_PERMISSIONS_CODE = 1
+    private val MAP_PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    private var map : MapView? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val ctx: Context = applicationContext
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
+
         setContentView(R.layout.activity_main)
+
+        requestPermissionsIfNecessary(MAP_PERMISSIONS)
+        initMap()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        map?.onResume()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        map?.onPause()
+    }
+
+    private fun initMap() {
+        map = findViewById(R.id.map)
+        map?.setTileSource(TileSourceFactory.MAPNIK)
+
+        val mapController = map?.controller
+        mapController?.setZoom(9.5)
+        val startPoint = GeoPoint(48.8583, 2.2944)
+        mapController?.setCenter(startPoint)
+    }
+
+    private fun requestPermissionsIfNecessary(permissions: Array<String>) {
+        val permissionsToRequest = permissions.filter { permission ->
+            (ActivityCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED)
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toTypedArray(),
+                    REQUEST_PERMISSIONS_CODE)
+        }
     }
 }
