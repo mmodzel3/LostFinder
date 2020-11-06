a4d2783b0328ab6781fa3afec182470f4371b35c
mmodzel3 <45737456+mmodzel3@users.noreply.github.com>
2020-11-06 19:04:09 +0100

    Added login activity layout.

---
+++ app/src/main/java/com/github/mmodzel3/lostfinder/security/authentication/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.github.mmodzel3.lostfinder.security.authentication.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mmodzel3.lostfinder.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+}
